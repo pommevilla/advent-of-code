c0dda46e5ee148053b1dcbec45d584fecade155e
Paul Villanueva <pvillanueva13@gmail.com>
2023-12-03 10:17:18 -0600

    Finish day 3

---
+++ python/day3.py
@@ -0,0 +1,105 @@
+import argparse
+from aoc_utils import part_header
+import re
+
+sample_input = "inputs/day3/day3_sample.txt"
+real_input = "inputs/day3/day3.txt"
+
+
+@part_header(part=1)
+def part_1(input_file: str, test: bool = False):
+    sum = 0
+
+    with open(input_file) as fin:
+        lines = [line.strip() for line in fin.readlines()]
+
+    for i, line in enumerate(lines):
+        for match in re.finditer(r"\d+", line):
+            # Adding cells to the left and right of the digits to look at
+            adjacent_idxs = [(i, match.start() - 1), (i, match.end())]
+
+            # Adding cells above and below the digits to look at
+            adjacent_idxs += [
+                (i - 1, j) for j in range(match.start() - 1, match.end() + 1)
+            ]
+            adjacent_idxs += [
+                (i + 1, j) for j in range(match.start() - 1, match.end() + 1)
+            ]
+
+            # For each adjacent cell, check if there's a non-"." character and if its
+            # within bounds
+            for a, b in adjacent_idxs:
+                if (
+                    0 <= a < len(lines)
+                    and 0 <= b < len(lines[a])
+                    and lines[a][b] != "."
+                ):
+                    # If we find one, add it to the sum and break the loop
+                    # print(f"\t{a}, {b}: {lines[a][b]}")
+                    # print(f"Adding {match.group()} to sum")
+                    sum += int(match.group())
+                    break
+
+    print(f"\n\tSolution: {sum}")
+
+    if test:
+        assert sum == 4361
+    else:
+        assert sum == 546312
+
+
+@part_header(part=2)
+def part_2(input_file: str, testing: bool):
+    from collections import defaultdict
+
+    sum = 0
+
+    # We'll keep track of which digits are adjacent to which symbols
+    adjacent_symbols = defaultdict(list)
+
+    with open(input_file) as fin:
+        lines = [line.strip() for line in fin.readlines()]
+
+    for i, line in enumerate(lines):
+        for match in re.finditer(r"\d+", line):
+            adjacent_idxs = [(i, match.start() - 1), (i, match.end())]
+            adjacent_idxs += [
+                (i - 1, j) for j in range(match.start() - 1, match.end() + 1)
+            ]
+            adjacent_idxs += [
+                (i + 1, j) for j in range(match.start() - 1, match.end() + 1)
+            ]
+
+            # If we find * in an adjacent cell, record the digit in the
+            # adjacent_symbols entry for the * position
+            for a, b in adjacent_idxs:
+                if (
+                    0 <= a < len(lines)
+                    and 0 <= b < len(lines[a])
+                    and lines[a][b] == "*"
+                ):
+                    adjacent_symbols[(a, b)].append(match.group())
+
+    for k, v in adjacent_symbols.items():
+        if len(v) == 2:
+            sum += int(v[0]) * int(v[1])
+
+    print(f"\n\tSolution: {sum}")
+
+    if testing:
+        assert sum == 467835
+    else:
+        assert sum == 87449461
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t", "--test", action="store_true", help="Whether or not to use the test input"
+    )
+    args = parser.parse_args()
+
+    input_file = sample_input if args.test else real_input
+
+    part_1(input_file, args.test)
+    part_2(input_file, args.test)
