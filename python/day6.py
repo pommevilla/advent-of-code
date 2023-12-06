5b15ef1a768df165ab0e6e6b7ad32fa8d7e8c017
Paul Villanueva <pvillanueva13@gmail.com>
2023-12-06 17:07:32 -0600

    Add day 6, day templates

---
+++ python/day6.py
@@ -0,0 +1,62 @@
+import argparse
+from aoc_utils import part_header
+
+sample_input = "inputs/day6/sample.txt"
+real_input = "inputs/day6/input.txt"
+
+
+@part_header(part=1)
+def part_1(input_file: str, testing: bool = False):
+    time, distance = [
+        [int(x) for x in x.split(":")[1].split()]
+        for x in open(input_file).read().split("\n")
+    ]
+
+    answer = 1
+    for t, d in zip(time, distance):
+        winners = 0
+        for i in range(t):
+            if (t - i) * i > d:
+                winners += 1
+        answer *= winners
+
+    print(f"\n\tSolution: {answer}")
+
+    if testing:
+        assert answer == 288
+    else:
+        assert answer == 281600
+
+
+@part_header(part=2)
+def part_2(input_file: str, testing: bool):
+    time, distance = [
+        int(x.split(":")[1].replace(" ", ""))
+        for x in open(input_file).read().split("\n")
+    ]
+
+    answer = 0
+
+    for i in range(time):
+        if (time - i) * i > distance:
+            answer += 1
+
+    print(f"\n\tSolution: {answer}")
+
+    if testing:
+        assert answer == 71503
+    else:
+        assert answer == 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t", "--test", action="store_true", help="Whether or not to use the test input"
+    )
+    args = parser.parse_args()
+
+    input_file = sample_input if args.test else real_input
+
+    part_1(input_file, args.test)
+    part_2(input_file, args.test)
---
+++ python/template.py
@@ -0,0 +1,42 @@
+import argparse
+from aoc_utils import part_header
+
+sample_input = "inputs/day6/sample.txt"
+real_input = "inputs/day6/input.txt"
+
+
+@part_header(part=1)
+def part_1(input_file: str, testing: bool = False):
+    sum = 0
+
+    print(f"\n\tSolution: {sum}")
+
+    if testing:
+        assert sum == 28
+    else:
+        assert sum == 0
+
+
+@part_header(part=2)
+def part_2(input_file: str, testing: bool):
+    sum = 0
+
+    print(f"\n\tNot yet implemented")
+
+    if testing:
+        assert sum == 0
+    else:
+        assert sum == 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-t", "--test", action="store_true", help="Whether or not to use the test input"
+    )
+    args = parser.parse_args()
+
+    input_file = sample_input if args.test else real_input
+
+    part_1(input_file, args.test)
+    part_2(input_file, args.test)
